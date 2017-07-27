1237a2e37bbf77926af9e95d3d62186ea8cbb33c
jean <jean.varela1@gmail.com>
2017-07-27 01:47:14 -0300

    Capturado a localização atual

--- app/src/main/java/br/com/contatos/visao/localizacao/LocalizaActivity.java
+++ app/src/main/java/br/com/contatos/visao/localizacao/LocalizaActivity.java
@@ -1,27 +1,31 @@
 package br.com.contatos.visao.localizacao;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.Toast;
 
 import br.com.contatos.R;
 import br.com.contatos.controlador.localizacao.ControladorLocalizacao;
@@ -33,26 +37,28 @@ import br.com.contatos.infraestrutura.webservice.ServiceGenerator;
 import br.com.contatos.infraestrutura.webservice.acao.AcaoGravarLocalizacao;
 import br.com.contatos.infraestrutura.webservice.interfaces.ErroHandler;
 import br.com.contatos.infraestrutura.webservice.interfaces.SucessoHandler;
-import br.com.contatos.infraestrutura.webservice.retorno.OcorrenciaRetorno;
 import br.com.contatos.infraestrutura.webservice.resource.OcorrenciaService;
+import br.com.contatos.infraestrutura.webservice.retorno.OcorrenciaRetorno;
 import br.com.contatos.modelo.entidade.localizacao.Localizacao;
 import br.com.contatos.visao.principal.PrincipalActivity;
 import retrofit2.Call;
 
-public class LocalizaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener,
-                                                                   GoogleApiClient.ConnectionCallbacks,
-                                                                   GoogleApiClient.OnConnectionFailedListener,
-                                                                   LocationListener, SucessoHandler,ErroHandler{
+public class LocalizaActivity extends AppCompatActivity implements LocationListener, SucessoHandler, ErroHandler {
 
     private Spinner spnClima;
     private GoogleApiClient googleApiClient;
     private LocationRequest locationRequest;
+    private String provider;
+
+    private LocationManager locationManager;
 
     private EditText txtLatitude;
     private EditText txtLongitude;
 
-    private long   idUsuario;
+    private long idUsuario;
     private String token;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,54 +75,48 @@ public class LocalizaActivity extends AppCompatActivity implements AdapterView.O
         txtLongitude = (EditText) findViewById(R.id.txtLongitude);
 
         Intent intent = getIntent();
-        idUsuario = intent.getLongExtra(Constantes.ID_USUARIO_PARAMETRO,-1);
+        idUsuario = intent.getLongExtra(Constantes.ID_USUARIO_PARAMETRO, -1);
         token = intent.getStringExtra(Constantes.ID_TOKEN_PARAMETRO);
 
-        criarConexaoGoogle();
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5, 5, this);
+        Location location = getLastKnownLocation();
+        if (location != null) {
+            onLocationChanged(location);
+        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private Location getLastKnownLocation() {
+        locationManager = (LocationManager)getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = locationManager.getProviders(true);
+        Location bestLocation = null;
 
-        if (googleApiClient != null && googleApiClient.isConnected()){
-            inicializaLocationUpdate();
+        for (String provider : providers) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            }
+            Location l = locationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+            }
         }
+        return bestLocation;
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-    }
-
-    private void criarConexaoGoogle(){
-        googleApiClient = new GoogleApiClient.Builder(this)
-                                             .addOnConnectionFailedListener(this)
-                                             .addConnectionCallbacks(this)
-                                             .addApi(LocationServices.API)
-                                             .build();
-        googleApiClient.connect();
-    }
-
-    private void inicializaLocationRequest(){
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(2000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-
-    private void inicializaLocationUpdate(){
-        inicializaLocationRequest();
-        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest,this);
-    }
-
-    private void stopLocationUpdate(){
-        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient,this);
+        locationManager.removeUpdates(this);
     }
 
     private void configuraSpinner(){
-        spnClima.setOnItemSelectedListener(this);
-
         List<String> categories = new ArrayList<String>();
         categories.add("Sol");
         categories.add("Chuva");
@@ -127,44 +127,7 @@ public class LocalizaActivity extends AppCompatActivity implements AdapterView.O
         spnClima.setAdapter(dataAdapter);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        if (location != null){
-            txtLatitude.setText(String.valueOf(location.getLatitude()));
-            txtLongitude.setText(String.valueOf(location.getLongitude()));
-        }
-
-        inicializaLocationUpdate();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        //TODO
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        //TODO
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if (location != null) {
-            txtLatitude.setText(String.valueOf(location.getLatitude()));
-            txtLongitude.setText(String.valueOf(location.getLongitude()));
-        }
-    }
 
     public void enviarLocalizacao(View view){
         if (Util.verificaConectado(this)) {
@@ -223,5 +186,23 @@ public class LocalizaActivity extends AppCompatActivity implements AdapterView.O
         intent.putExtra(Constantes.ID_USUARIO_PARAMETRO,idUsuario);
         intent.putExtra(Constantes.ID_TOKEN_PARAMETRO,token);
         startActivity(intent);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        txtLatitude.setText(String.valueOf(location.getLatitude()));
+        txtLongitude.setText(String.valueOf(location.getLongitude()));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
     }
 }
